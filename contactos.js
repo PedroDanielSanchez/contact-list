b36231821df00ff8051ee7b3d19f98df87c3ce92
Pedro Daniel Sanchez <nexcall@gmail.com>
2022-10-10 22:30:46 -0500

    Changed to onject elements in the array

--- contactos.js
+++ contactos.js
@@ -1,34 +1,60 @@
 // Author: Pedro Sanchez
 
 const contactos = [
-    "Pedro Sanchez Amaro",
-    "Angelina Sanchez",
-    "Mario Jimenez",
-    "Nicole Sanchez"
+  {
+    id: 0,
+    nombre: "Pedro",
+    apellidos: "Sanchez Amaro",
+    telefono: "8098787078",
+    ubicaciones: [
+        {ciudad: "Medellin",
+         direccion: "Calle ABC"
+        },
+        {ciudad: "Bogota",
+         direccion: "Parque de la 93"
+        }
+    ]
+  },
+  {
+        id: 1,
+        nombre: "Angelina",
+        apellidos: "Sanchez",
+        telefono: "222222",
+        ubicaciones: [
+            {
+                ciudad: "Medellin",
+                direccion: "Calle 2022 No 11"
+            },
+            {
+                ciudad: "Ciudad Brillante",
+                direccion: "Calle Eterna Infinita"
+            }
+        ]
+  },
+
 ]
 
-const borrarElemento = (lista, nombre) => {
-    let pos = lista.indexOf(nombre);
-  if (pos > -1) {
-    lista.splice(pos, 1)
+// id is the index position
+const borrarElemento = (lista, id) => {
+    lista.splice(id, 1)
   }
-}
 
-const adicinaElemento = (lista, nombre) => {
+
+// persona must be a json object
+const adicinaElemento = (lista, persona) => {
     lista.push(nombre);
 }
 
 const displayElements = (lista) => {
   for (i=0; i<lista.length; i++) {
-    console.log(`${i+1}: ${lista[i]}`);
+    console.log(`${i+1}: ${JSON.stringify(lista[i])}`);
   }
   console.log(`\n`);
 }
 
 displayElements(contactos);
-borrarElemento(contactos, "Mario Jimenez");
+borrarElemento(contactos, 0);
 displayElements(contactos);
-adicinaElemento(contactos,"Celeste Sanchez");
-displayElements(contactos);
+
 
 
